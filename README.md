60320f2e9b0c6f6728315e9272a20eb52133c288
Tutch <lucasmapurunga@gmail.com>
2017-11-15 18:50:38 -0300

    Individual relationship graphs added. Legends added. Dropdown for individual entity selection added. Removed previous all-for-all graph

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dwarven-relationship
 Builds a graph showing entities relationship in a given site [WIP]
+
+## How to use
+1. Save your game and retire your fort
+2. Create a Legends save on your world
+3. Create legends.xml dump 
+4. Open the generated file. On the very first line, you have to change where it says ```encoding="CP437"``` to ```encoding="utf-8"```
+5. Save it as another file
+6. Open it on the tool
+7. Enter the english name of the fortress. Or any other site, really.
--- js/relationship_graph.js
+++ js/relationship_graph.js
@@ -166,3 +166,88 @@ RelationshipGraph.prototype.drawSingleGraph = function(element, entities, dwarve
       graph.showLegend();
   });
 }
+
+
+
+/*
+function RelationshipGraph(element, entities, idList, dwarvenNamesIds) {
+  // create a network
+  var container = document.getElementById(element);
+
+  // Each entity node
+  var nodes='';
+  // All edges
+  var edges='';
+
+  var femaleNode = "[color=#db8fd3 fillcolor=#db8fd3]";
+  var maleNode   = "[color=#6fb8e6 fillcolor=#6fb8e6]";
+
+  for(i=0; i<entities.length; i++) {
+    nodes += '\"'+ toTitleCase(entities[i].name) +'\"';
+    if(entities[i].caste == "FEMALE") {
+      nodes+= femaleNode + ";";
+    }else{
+      nodes+= maleNode + ";";
+    }
+
+    for(j=0; j<entities[i].relationships.length; j++) {
+      //console.log(entities[i].relationships[j].hfid);
+      if(idList.includes(entities[i].relationships[j].hfid)) {
+        var result = dwarvenNamesIds.filter(x => x.id === entities[i].relationships[j].hfid);
+
+        edges += '\"'+ toTitleCase(entities[i].name) + '\" -- \"'+ toTitleCase(result[0].name) +'\";';
+      }
+    }
+
+  }
+
+  var DOTstring = 'network {'+ nodes + edges +'}';
+  var parsedData = vis.network.convertDot(DOTstring);
+
+  var data = {
+    nodes: parsedData.nodes,
+    edges: parsedData.edges
+  }
+
+  var options = parsedData.options;
+
+  options.autoResize = true;
+
+  // you can extend the options like a normal JSON variable:
+  options.nodes = {
+     shape: 'box',
+     margin: 5,
+     font: {
+       multi: true
+     }
+  }
+
+  var calcLength = 300 + entities.length * 10;
+
+  options.edges = {
+    length: calcLength,
+    //physics:false
+  }
+
+  options.layout = {
+      improvedLayout: true
+  }
+
+  options.physics = {
+    //enabled: false,
+    adaptiveTimestep: true
+    //stabilization: false
+  }
+
+  document.getElementById('loading').style.display = 'table';
+
+  // create a network
+  var network = new vis.Network(container, data, options);
+
+  network.once("afterDrawing", function() {
+      console.log('im back, bitch');
+      document.getElementById('loading').style.display = 'none';
+  });
+}
+
+*/
